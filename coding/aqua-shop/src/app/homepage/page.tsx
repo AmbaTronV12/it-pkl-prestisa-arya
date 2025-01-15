15425ac38eef1b0cd95bff606544ac908eb5df9d
AmbaTronV12 <indraarya0181@gmail.com>
2025-01-15 22:35:18 +0800

    progress hari rabu

--- coding/aqua-shop/src/app/homepage/page.tsx
+++ coding/aqua-shop/src/app/homepage/page.tsx
@@ -1,13 +1,39 @@
 import React from 'react'
-import styles from './home.module.css'
+import styles from './css/home.module.css'
 import { Navbar } from '@/component'
+import { heroImage } from '@/public/assets'
+import Image from 'next/image'
 
 const home = () => {
-  return (
-    <div>
-      <Navbar></Navbar>
+return (
+<div className={styles.page}>
+  <Navbar></Navbar>
+  <div className={styles.hero}>
+    <Image src={heroImage} alt='hero' />
+    <div className={styles.heroText}>
+      <h1>DIVE INTO EXCELLENCE</h1>
+      <h2>SHOP NOW</h2>
     </div>
-  )
+  </div>
+  <div className={styles.collection}>
+    <h1>OUR COLLECTION</h1>
+    <div className={styles.collectionGrid}>
+      <div className={styles.collection1}>
+        <div className={styles.menCollection}></div>
+        <div className={styles.childrenCollection}></div>
+      </div>
+      <div className={styles.collection2}>
+        <div className={styles.googlesCollection}>
+        </div>
+        <div className={styles.womenCollection}></div>
+      </div>
+      <div className={styles.sponsor}>
+        <div></div>
+      </div>
+    </div>
+  </div>
+</div>
+)
 }
 
 
