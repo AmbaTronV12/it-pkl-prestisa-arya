063e620bb6b01b006fa7978da4b30426a6aebf87
AmbaTronV12 <indraarya0181@gmail.com>
2025-01-16 21:40:43 +0800

    progress hari kamis

--- coding/aqua-shop/src/app/homepage/page.tsx
+++ coding/aqua-shop/src/app/homepage/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './css/home.module.css'
 import { Navbar } from '@/component'
-import { heroImage } from '@/public/assets'
+import { heroImage, calebImage, zacImage } from '@/public/assets'
 import Image from 'next/image'
 
 const home = () => {
@@ -16,20 +16,61 @@ return (
     </div>
   </div>
   <div className={styles.collection}>
-    <h1>OUR COLLECTION</h1>
-    <div className={styles.collectionGrid}>
-      <div className={styles.collection1}>
-        <div className={styles.menCollection}></div>
-        <div className={styles.childrenCollection}></div>
-      </div>
-      <div className={styles.collection2}>
-        <div className={styles.googlesCollection}>
+    <div className={styles.collectionContainer}>
+      <h1>OUR COLLECTION</h1>
+      <div className={styles.collectionGrid}>
+        <div className={styles.collection1}>
+          <div className={styles.menCollection}>
+            <p>MEN'S SWIMWEAR</p>
+          </div>
+          <div className={styles.childrenCollection}>
+            <p>CHILDREN'S<br /> SWIMWEAR</p>
+          </div>
         </div>
-        <div className={styles.womenCollection}></div>
+        <div className={styles.collection2}>
+          <div className={styles.googlesCollection}>
+            <p>CRYSTAL VISION GOOGLES</p>
+          </div>
+          <div className={styles.womenCollection}>
+            <p>WOMAN'S<br />SWIMWEAR</p>
+          </div>
+        </div>
+        <div className={styles.sponsor}>
+          <h2>SPONSOR</h2>
+          <div className={styles.sponsorProfile}>
+            <div>
+            <h4>CAELEB DRESSEL</h4>
+            <p>SWIMMING ATHLETE</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className={styles.sponsor}>
-        <div></div>
-      </div>
+    </div>
+  </div>
+  <div className={styles.extras}>
+    <div className={styles.overflowTop}>
+    <div className={styles.extrasImage}>
+    <div className={styles.imagesDown}>
+      <Image src={calebImage} alt='calebExtra'/>
+      <div className={styles.extrasImageLine}></div>
+    </div>
+    <div className={styles.imagesUp}>
+      <Image src={zacImage} alt='zacExtra'/>
+      <div className={styles.extrasImageLine}></div>
+    </div>
+    </div>
+    <div className={styles.extrasText}>
+      <p className={styles.extrasTitle}>
+        <span className={styles.overflowTitle}>DIVE INTO EXCELLENCE:</span>
+        <br/>
+        EVERYTHING YOU NEED FOR SWIMMING!</p>
+      <p className={styles.extrasDesc}>
+        At Aqua Shop, we provide top-quality 
+        swimming gear for everyone—from beginners to pros. 
+        Our mission is to enhance your swimming 
+        experience with reliable, stylish, and durable products.
+        </p>
+    </div>
     </div>
   </div>
 </div>
--- coding/aqua-shop/src/public/assets/index.ts
+++ coding/aqua-shop/src/public/assets/index.ts
@@ -3,3 +3,5 @@ export {default as seacrhIcon} from './Seacrh.svg';
 export {default as cartIcon} from './bag.svg';
 export {default as profileIcon} from './frame.svg';
 export {default as heroImage} from './Hero.png';
+export {default as calebImage} from './dresselV2.png';
+export {default as zacImage} from './zac.png';
